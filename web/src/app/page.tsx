4334061e5bee7abf77b5541ca9d681d2c7d7a3d5
oCaioAug <ocaioaug.dev@protonmail.com>
2025-09-14 11:30:14 -0300

    initial commit of web and mobile apps

---
+++ web/src/app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Button className="bg-blue-500 text-white">Olá mundo</Button>
+  );
+}
